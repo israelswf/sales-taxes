935a2569a4b0032ea0ec17f3fbb915cb716ba884
Israel Costa <icc@cin.ufpe.br>
2020-02-28 07:16:56 -0300

    Enable CrossOrigin with React on localhost:3000

--- sales-taxes-backend/src/main/java/com/liferay/salestaxes/controller/CartController.java
+++ sales-taxes-backend/src/main/java/com/liferay/salestaxes/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.liferay.salestaxes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +17,13 @@ public class CartController {
 	@Autowired
 	CartService cartService;
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/cart")
 	public Cart getCart() {
 		return cartService.getCart();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/cart/items") 
 	public Cart addItem(@RequestBody Item item) {
 		
